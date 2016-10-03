daa43fd1a8c8101220210ad69ace515d571e2b02
Jonathan Kay <jokay@redhat.com>
2016-10-03 14:22:40 -0400

    quickstart script and jboss eap added to favorites

---
+++ startup.sh
@@ -0,0 +1,5 @@
+cp .bashrc ~/.bashrc
+cp .vimrc ~/.vimrc
+cp inputrc /etc/inputrc
+cp jbdevstudio.desktop ~/.local/share/applications
+dconf write /org/gnome/shell/overrides/button-layout 'close,maximize,minimize:'
