96ffa629c9ddab66dd93dd61af42a613ccee534f
Jonathan Kay <jokay@redhat.com>
2016-10-03 15:57:56 -0400

    background image

--- startup.sh
+++ startup.sh
@@ -3,4 +3,5 @@ cp .vimrc ~/.vimrc
 cp inputrc /etc/inputrc
 cp jbdevstudio.desktop ~/.local/share/applications
 cp -r system-connections /etc/NetworkManager
+cp coloroty.jpg ~/Pictures
 dconf write /org/gnome/shell/overrides/button-layout 'close,maximize,minimize:'
