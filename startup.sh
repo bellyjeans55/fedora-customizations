5fa898ce05581d5dd3fe6527fb648f939095bd04
Jonathan Kay <jokay@redhat.com>
2016-10-03 16:04:12 -0400

    devilspie2 setup

--- startup.sh
+++ startup.sh
@@ -4,4 +4,6 @@ cp inputrc /etc/inputrc
 cp jbdevstudio.desktop ~/.local/share/applications
 cp -r system-connections /etc/NetworkManager
 cp coloroty.jpg ~/Pictures
+cp -p ~/.config/devilspie2/terminal_prefs.lua
+cp devilspie2.desktop ~/.config/autostart
 dconf write /org/gnome/shell/overrides/button-layout 'close,maximize,minimize:'
---
+++ terminal_prefs.lua
@@ -0,0 +1,3 @@
+if (get_window_name()=="Terminal") then
+    set_window_position(1000,0);
+end
