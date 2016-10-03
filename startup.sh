73b6e2b28422919a438fe035ab59062ee88d5dee
Jonathan Kay <jokay@redhat.com>
2016-10-03 15:54:04 -0400

    Added VPN configurations

--- startup.sh
+++ startup.sh
@@ -2,4 +2,5 @@ cp .bashrc ~/.bashrc
 cp .vimrc ~/.vimrc
 cp inputrc /etc/inputrc
 cp jbdevstudio.desktop ~/.local/share/applications
+cp -r system-connections /etc/NetworkManager
 dconf write /org/gnome/shell/overrides/button-layout 'close,maximize,minimize:'
